83b3e4ed044df2e335fca2ebeac3c78574a120b5
quang2005asd <quangcokhi2005@gmail.com>
2025-12-01 23:09:04 +0700

    fix lỗi up

--- DNU.CanteenConnect.Web/obj/Debug/net9.0/DNU.CanteenConnect.Web.AssemblyInfo.cs
+++ DNU.CanteenConnect.Web/obj/Debug/net9.0/DNU.CanteenConnect.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNU.CanteenConnect.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8dde9656bd05681fe3c1b517169394e946b062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346e85927e9c9d3c60996a1ab9b194846ab56fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNU.CanteenConnect.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNU.CanteenConnect.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
